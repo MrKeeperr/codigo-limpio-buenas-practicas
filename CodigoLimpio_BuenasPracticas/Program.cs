1650e1f9d939004647406bfd705a29576bcae6c5
dbitz <dbielosp@gmail.com>
2025-07-02 12:52:57 -0500

    Refactor: put duplicated code in one method

--- CodigoLimpio_BuenasPracticas/Program.cs
+++ CodigoLimpio_BuenasPracticas/Program.cs
@@ -53,11 +53,7 @@ namespace ToDo
             {
                 Console.WriteLine("Ingrese el número de la tarea a remover: ");
                 // Show current taks
-                for (int i = 0; i < TaskList.Count; i++)
-                {
-                    Console.WriteLine((i + 1) + ". " + TaskList[i]);
-                }
-                Console.WriteLine("----------------------------------------");
+                ShowTaskList();
 
                 string removerTarea = Console.ReadLine();
                 // Remove one position
@@ -99,12 +95,16 @@ namespace ToDo
             }
             else
             {
-                Console.WriteLine("----------------------------------------");
-                for (int i = 0; i < TaskList.Count; i++)
-                {
-                    Console.WriteLine((i + 1) + ". " + TaskList[i]);
-                }
-                Console.WriteLine("----------------------------------------");
+                ShowTaskList();
+            }
+        }
+
+        private static void ShowTaskList()
+        {
+            Console.WriteLine("----------------------------------------");
+            for (int i = 0; i < TaskList.Count; i++)
+            {
+                Console.WriteLine((i + 1) + ". " + TaskList[i]);
             }
         }
     }
